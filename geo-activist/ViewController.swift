db712aa82643a09eb4fa01a60dbbae9065264927
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2020-01-06 16:06:24 +0900

    Initial Commit

---
+++ geo-activist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  geo-activist
+//
+//  Created by 鎌田遼 on 2020/01/06.
+//  Copyright © 2020 鎌田遼. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
